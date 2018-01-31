e15b47b3a10f5dadbfd3b0c92ca858a68cf43cb6
Jacob Killelea <Jkillelea@protonmail.ch>
2018-01-31 12:29:09 -0700

    misc

--- main.m
+++ main.m
@@ -1,4 +1,5 @@
 % ASEN 3128, Jacob Killelea, HW 2
+% Quadcopter data visualization
 clear; close all; clc;
 
 load('RSdata.mat'); % [rt_tout, rt_yout]
@@ -10,26 +11,35 @@ signals = rt_yout.signals;
 % <r>, [empty], [empty], <ddx>, <ddy>, <ddz>, <p>, <q>, <r>, <altitude_sonar>,
 % <prs>, [empty], [empty], [empty], [empty], [empty], [empty]
 
-x = getDataByLabel(signals, '<X>');
-y = getDataByLabel(signals, '<Y>');
-z = getDataByLabel(signals, '<Z>');
+x   = getDataByLabel(signals, '<X>');
+y   = getDataByLabel(signals, '<Y>');
+z   = getDataByLabel(signals, '<Z>');
 
-dx = getDataByLabel(signals, '<dx>');
-dy = getDataByLabel(signals, '<dy>');
-dz = getDataByLabel(signals, '<dz>');
+dx  = getDataByLabel(signals, '<dx>');
+dy  = getDataByLabel(signals, '<dy>');
+dz  = getDataByLabel(signals, '<dz>');
 
 ddx = getDataByLabel(signals, '<ddx>');
 ddy = getDataByLabel(signals, '<ddy>');
 ddz = getDataByLabel(signals, '<ddz>');
 
+z = -z;
+dz = -dz;
+ddz = -ddz;
+
 % print out flightpath
 figure; hold on;
 plot3(x, y, z)
 plot3(x(1), y(1), z(1), 'ro')
+xlabel('x')
+ylabel('y')
+zlabel('z')
+xlim([-1, 1])
+ylim([-1, 1])
 
 % velocity vectors
-for i = 1:10:length(dx)
-  line( [x(i), x(i) + 0.1*dx(i)], ...
-        [y(i), y(i) + 0.1*dy(i)], ...
-        [z(i), z(i) + 0.1*dz(i)])
-end
+% for i = 1:10:length(dx)
+%   line( [x(i), x(i) + 0.1*dx(i)], ...
+%         [y(i), y(i) + 0.1*dy(i)], ...
+%         [z(i), z(i) + 0.1*dz(i)])
+% end
