caa8f3a216f547a2d4758fff6d1b5f6dea424956
Jacob Killelea <Jkillelea@protonmail.ch>
2018-02-12 14:42:57 -0700

    nonlinearized motor control

--- main.m
+++ main.m
@@ -1,13 +1,13 @@
-% ASEN 3128, Jacob Killelea, Assignment 3
+% ASEN 3128, Jacob Killelea, Assignment 4
 % main script for the nonlinear quadcopter sim
 clear
 clc
 close all
 addpath(genpath('util')); % utility functions
 
-tmax = 50; % seconds
+tmax = 5; % seconds
 
-% Problem 2a: +5 degrees bank
+% Problem 3a: +5 degrees bank
 x0     = [0 0 0];          % position [N, E, D]
 vel0   = [0 0 0];          % body frame velocity
 pose0  = [deg2rad(5) 0 0]; % phi, theta, psi
@@ -15,14 +15,14 @@ omega0 = [0 0 0];          % body angular velocity
 inital_conds = [x0, vel0, pose0, omega0];
 
 opts = struct('aero',          true, ... % aerodynamic forces
-              'control_motor', false);   % just counteract weight
+              'control_motor', true);   % full motor control
 % call ode45
 [t, y] = ode45(@(t, y) quadcopter(t, y, opts), [0, tmax], inital_conds);
 plotQuadCopterDynamics(t, y);
-print(['img/', 'prob2a'], '-dpng');
+print(['img/', 'prob3a'], '-dpng');
 close
 
-% Problem 2b: +5 degrees pitch
+% Problem 3b: +5 degrees pitch
 x0     = [0 0 0];          % position
 vel0   = [0 0 0];          % body frame velocity
 pose0  = [0 deg2rad(5) 0]; % phi, theta, psi
@@ -30,29 +30,14 @@ omega0 = [0 0 0];          % body angular velocity
 inital_conds = [x0, vel0, pose0, omega0];
 
 opts = struct('aero',          true, ... % aerodynamic forces
-              'control_motor', false);   % just counteract weight
+              'control_motor', true);   % full motor control
 % call ode45
 [t, y] = ode45(@(t, y) quadcopter(t, y, opts), [0, tmax], inital_conds);
 plotQuadCopterDynamics(t, y);
-print(['img/', 'prob2b'], '-dpng');
+print(['img/', 'prob3b'], '-dpng');
 close
 
-% Problem 2c: +5 degrees azimuth
-x0     = [0 0 0];          % position
-vel0   = [0 0 0];          % body frame velocity
-pose0  = [0 0 deg2rad(5)]; % phi, theta, psi
-omega0 = [0 0 0];          % body angular velocity
-inital_conds = [x0, vel0, pose0, omega0];
-
-opts = struct('aero',          true, ... % aerodynamic forces
-              'control_motor', false);   % just counteract weight
-% call ode45
-[t, y] = ode45(@(t, y) quadcopter(t, y, opts), [0, tmax], inital_conds);
-plotQuadCopterDynamics(t, y);
-print(['img/', 'prob2c'], '-dpng');
-close
-
-% Problem 2d: +0.1 rad/sec roll rate
+% Problem 3c: +0.1 rad/sec roll rate
 x0     = [0 0 0];   % position
 vel0   = [0 0 0];   % body frame velocity
 pose0  = [0 0 0];   % phi, theta, psi
@@ -60,14 +45,14 @@ omega0 = [0.1 0 0]; % body angular velocity
 inital_conds = [x0, vel0, pose0, omega0];
 
 opts = struct('aero',          true, ... % aerodynamic forces
-              'control_motor', false);   % just counteract weight
+              'control_motor', true);   % full motor control
 % call ode45
 [t, y] = ode45(@(t, y) quadcopter(t, y, opts), [0, tmax], inital_conds);
 plotQuadCopterDynamics(t, y);
-print(['img/', 'prob2d'], '-dpng');
+print(['img/', 'prob3c'], '-dpng');
 close
 
-% Problem 2e: +0.1 rad/sec pitch rate
+% Problem 3d: +0.1 rad/sec pitch rate
 x0     = [0 0 0];   % position
 vel0   = [0 0 0];   % body frame velocity
 pose0  = [0 0 0];   % phi, theta, psi
@@ -75,69 +60,9 @@ omega0 = [0 0.1 0]; % body angular velocity
 inital_conds = [x0, vel0, pose0, omega0];
 
 opts = struct('aero',          true, ... % aerodynamic forces
-              'control_motor', false);   % just counteract weight
+              'control_motor', true);   % full motor control
 % call ode45
 [t, y] = ode45(@(t, y) quadcopter(t, y, opts), [0, tmax], inital_conds);
 plotQuadCopterDynamics(t, y);
-print(['img/', 'prob2e'], '-dpng');
-close
-
-% Problem 2f: +0.1 rad/sec yaw rate
-x0     = [0 0 0];   % position
-vel0   = [0 0 0];   % body frame velocity
-pose0  = [0 0 0];   % phi, theta, psi
-omega0 = [0 0 0.1]; % body angular velocity
-inital_conds = [x0, vel0, pose0, omega0];
-
-opts = struct('aero',          true, ... % aerodynamic forces
-              'control_motor', false);   % just counteract weight
-% call ode45
-[t, y] = ode45(@(t, y) quadcopter(t, y, opts), [0, tmax], inital_conds);
-plotQuadCopterDynamics(t, y);
-print(['img/', 'prob2f'], '-dpng');
-close
-
-% Problem 4d: +0.1 rad/sec roll rate
-x0     = [0 0 0];   % position
-vel0   = [0 0 0];   % body frame velocity
-pose0  = [0 0 0];   % phi, theta, psi
-omega0 = [0.1 0 0]; % body angular velocity
-inital_conds = [x0, vel0, pose0, omega0];
-
-opts = struct('aero',          true, ... % aerodynamic forces
-              'control_motor', true);   % just counteract weight
-% call ode45
-[t, y] = ode45(@(t, y) quadcopter(t, y, opts), [0, tmax], inital_conds);
-plotQuadCopterDynamics(t, y);
-print(['img/', 'prob4d'], '-dpng');
-close
-
-% Problem 4e: +0.1 rad/sec pitch rate
-x0     = [0 0 0];   % position
-vel0   = [0 0 0];   % body frame velocity
-pose0  = [0 0 0];   % phi, theta, psi
-omega0 = [0 0.1 0]; % body angular velocity
-inital_conds = [x0, vel0, pose0, omega0];
-
-opts = struct('aero',          true, ... % aerodynamic forces
-              'control_motor', true);   % just counteract weight
-% call ode45
-[t, y] = ode45(@(t, y) quadcopter(t, y, opts), [0, tmax], inital_conds);
-plotQuadCopterDynamics(t, y);
-print(['img/', 'prob4e'], '-dpng');
-close
-
-% Problem 4f: +0.1 rad/sec yaw rate
-x0     = [0 0 0];   % position
-vel0   = [0 0 0];   % body frame velocity
-pose0  = [0 0 0];   % phi, theta, psi
-omega0 = [0 0 0.1]; % body angular velocity
-inital_conds = [x0, vel0, pose0, omega0];
-
-opts = struct('aero',          true, ... % aerodynamic forces
-              'control_motor', true);   % just counteract weight
-% call ode45
-[t, y] = ode45(@(t, y) quadcopter(t, y, opts), [0, tmax], inital_conds);
-plotQuadCopterDynamics(t, y);
-print(['img/', 'prob4f'], '-dpng');
+print(['img/', 'prob3d'], '-dpng');
 close
