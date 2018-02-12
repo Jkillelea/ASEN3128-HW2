c45e37d2f28ea5cd479334b20ff28c30551f4ca9
Jacob Killelea <Jkillelea@protonmail.ch>
2018-02-12 00:24:44 -0700

    move motor control to nonlinear sim

--- main.m
+++ main.m
@@ -96,3 +96,48 @@ opts = struct('aero',          true, ... % aerodynamic forces
 plotQuadCopterDynamics(t, y);
 print(['img/', 'prob2f'], '-dpng');
 close
+
+% Problem 4d: +0.1 rad/sec roll rate
+x0     = [0 0 0];   % position
+vel0   = [0 0 0];   % body frame velocity
+pose0  = [0 0 0];   % phi, theta, psi
+omega0 = [0.1 0 0]; % body angular velocity
+inital_conds = [x0, vel0, pose0, omega0];
+
+opts = struct('aero',          true, ... % aerodynamic forces
+              'control_motor', true);   % just counteract weight
+% call ode45
+[t, y] = ode45(@(t, y) quadcopter(t, y, opts), [0, tmax], inital_conds);
+plotQuadCopterDynamics(t, y);
+print(['img/', 'prob4d'], '-dpng');
+close
+
+% Problem 4e: +0.1 rad/sec pitch rate
+x0     = [0 0 0];   % position
+vel0   = [0 0 0];   % body frame velocity
+pose0  = [0 0 0];   % phi, theta, psi
+omega0 = [0 0.1 0]; % body angular velocity
+inital_conds = [x0, vel0, pose0, omega0];
+
+opts = struct('aero',          true, ... % aerodynamic forces
+              'control_motor', true);   % just counteract weight
+% call ode45
+[t, y] = ode45(@(t, y) quadcopter(t, y, opts), [0, tmax], inital_conds);
+plotQuadCopterDynamics(t, y);
+print(['img/', 'prob4e'], '-dpng');
+close
+
+% Problem 4f: +0.1 rad/sec yaw rate
+x0     = [0 0 0];   % position
+vel0   = [0 0 0];   % body frame velocity
+pose0  = [0 0 0];   % phi, theta, psi
+omega0 = [0 0 0.1]; % body angular velocity
+inital_conds = [x0, vel0, pose0, omega0];
+
+opts = struct('aero',          true, ... % aerodynamic forces
+              'control_motor', true);   % just counteract weight
+% call ode45
+[t, y] = ode45(@(t, y) quadcopter(t, y, opts), [0, tmax], inital_conds);
+plotQuadCopterDynamics(t, y);
+print(['img/', 'prob4f'], '-dpng');
+close
--- motor_control.m
+++ motor_control.m
@@ -1,11 +1,38 @@
 function [f1, f2, f3, f4] = motor_control(m, g, state)
   d = 0.06;      % m
-  r = d/sqrt(2); % lateral distance from fan to CM, m
+  r_dist = d/sqrt(2); % lateral distance from fan to CM, m
   k = 0.0024;
+  gain = 0.004;
 
-  force = 0.6666128353; % N
-  f1 = force/4;
-  f2 = force/4;
-  f3 = force/4;
-  f4 = force/4;
+  omega = state(10:12); % body angular velocity
+  p = omega(1);
+  q = omega(2);
+  r = omega(3);
+
+  f1 = m*g/4;
+  f2 = m*g/4;
+  f3 = m*g/4;
+  f4 = m*g/4;
+
+  % Control moments are 0.004*rotation_rate in their respoective directions
+  gain = 0.004;
+
+  matrix = [(r_dist)*[-1  1  1 -1];
+            (r_dist)*[-1 -1  1  1];
+                 (k)*[-1  1 -1  1];
+                     [-1 -1 -1 -1]];
+
+  torqe_command = -gain*[ p;
+                          q;
+                          r;
+                          0 ]; % desired Z control force - zero.
+                               % note that the control force that counters weight
+                               % was accounted for in the original linearization
+                               % of the equation set
+  delta_motors = matrix\torqe_command;
+
+  f1 = f1 + delta_motors(1);
+  f2 = f2 + delta_motors(2);
+  f3 = f3 + delta_motors(3);
+  f4 = f4 + delta_motors(4);
 end
