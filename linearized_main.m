ba8f881ff5bf04d172960d70a76ee12becf10512
Jacob Killelea <Jkillelea@protonmail.ch>
2018-02-11 18:26:09 -0700

    same set of disturbances on the linearized and nonlinear models

--- linearized_main.m
+++ linearized_main.m
@@ -5,7 +5,7 @@ clc
 close all
 addpath(genpath('util'));
 
-tmax = 1000; % seconds
+tmax = 100; % seconds
 
 %%% Conditions for steady hover
 % inital position, velocity
@@ -15,29 +15,164 @@ pose0  = [0 0 0]; % phi, theta, psi
 omega0 = [0 0 0]; % body angular velocity
 hover_conditions = [x0, vel0, pose0, omega0];
 
-%%% Initial deviations from hover conditions
-% inital position, velocity
+% problem 3a: +5 degrees bank
+% inital position deviation, velocity deviation
 deltaPos0   = [0 0 0]; % inertial displacement
 deltaVel0   = [0 0 0]; % body frame velocity
-deltaPose0  = [0 0 0]; % phi, theta, psi
+deltaPose0  = [deg2rad(5) 0 0]; % phi, theta, psi
 deltaOmega0 = [0 0 0]; % body angular velocity
 inital_conds = [deltaPos0, deltaVel0, deltaPose0, deltaOmega0];
-
 % there are currently no options implemented on the linearized model
-opts = struct('aero',          false, ... % no aerodynamic forces
-              'control_motor', false);    % no motor control
-
+opts = struct('aero',          true, ... % aerodynamic forces
+              'control_motor', false);   % no motor control
 % call ode45 on the linearized model
-[t, y] = ode45(@(t, y) linear_quadcopter(t, y, opts), [0, tmax], inital_conds);
-
+[t, y] = ode45(@(t, y) linear_quadcopter(t, y, opts, hover_conditions), [0, tmax], inital_conds);
 % deviations from hover over time
 deltaPos   = y(:, 1:3);
 deltaVel   = y(:, 4:6);
 deltaPose  = y(:, 7:9);
 deltaOmega = y(:, 10:12);
-
 % recover absolue values over time
 x     = x0     + deltaPos;
 vel   = vel0   + deltaVel;
 pose  = pose0  + deltaPose;
 omega = omega0 + deltaOmega;
+
+plotQuadCopterDynamics(t, [x, vel, pose, omega])
+print(['img/', 'prob3a'], '-dpng')
+close
+
+% problem 3b: +5 degrees pitch
+% inital position deviation, velocity deviation
+deltaPos0   = [0 0 0]; % inertial displacement
+deltaVel0   = [0 0 0]; % body frame velocity
+deltaPose0  = [0 deg2rad(5) 0]; % phi, theta, psi
+deltaOmega0 = [0 0 0]; % body angular velocity
+inital_conds = [deltaPos0, deltaVel0, deltaPose0, deltaOmega0];
+% there are currently no options implemented on the linearized model
+opts = struct('aero',          true, ... % aerodynamic forces
+              'control_motor', false);   % no motor control
+% call ode45 on the linearized model
+[t, y] = ode45(@(t, y) linear_quadcopter(t, y, opts, hover_conditions), [0, tmax], inital_conds);
+% deviations from hover over time
+deltaPos   = y(:, 1:3);
+deltaVel   = y(:, 4:6);
+deltaPose  = y(:, 7:9);
+deltaOmega = y(:, 10:12);
+% recover absolue values over time
+x     = x0     + deltaPos;
+vel   = vel0   + deltaVel;
+pose  = pose0  + deltaPose;
+omega = omega0 + deltaOmega;
+
+plotQuadCopterDynamics(t, [x, vel, pose, omega])
+print(['img/', 'prob3b'], '-dpng')
+close
+
+% problem 3c: +5 degrees azimuth
+% inital position deviation, velocity deviation
+deltaPos0   = [0 0 0]; % inertial displacement
+deltaVel0   = [0 0 0]; % body frame velocity
+deltaPose0  = [0 0 deg2rad(5)]; % phi, theta, psi
+deltaOmega0 = [0 0 0]; % body angular velocity
+inital_conds = [deltaPos0, deltaVel0, deltaPose0, deltaOmega0];
+% there are currently no options implemented on the linearized model
+opts = struct('aero',          true, ... % aerodynamic forces
+              'control_motor', false);   % no motor control
+% call ode45 on the linearized model
+[t, y] = ode45(@(t, y) linear_quadcopter(t, y, opts, hover_conditions), [0, tmax], inital_conds);
+% deviations from hover over time
+deltaPos   = y(:, 1:3);
+deltaVel   = y(:, 4:6);
+deltaPose  = y(:, 7:9);
+deltaOmega = y(:, 10:12);
+% recover absolue values over time
+x     = x0     + deltaPos;
+vel   = vel0   + deltaVel;
+pose  = pose0  + deltaPose;
+omega = omega0 + deltaOmega;
+
+plotQuadCopterDynamics(t, [x, vel, pose, omega])
+print(['img/', 'prob3c'], '-dpng')
+close
+
+% problem 3d: +0.1 rad/sec roll rate
+% inital position deviation, velocity deviation
+deltaPos0   = [0 0 0]; % inertial displacement
+deltaVel0   = [0 0 0]; % body frame velocity
+deltaPose0  = [0 0 0]; % phi, theta, psi
+deltaOmega0 = [0.1 0 0]; % body angular velocity
+inital_conds = [deltaPos0, deltaVel0, deltaPose0, deltaOmega0];
+% there are currently no options implemented on the linearized model
+opts = struct('aero',          true, ... % aerodynamic forces
+              'control_motor', false);   % no motor control
+% call ode45 on the linearized model
+[t, y] = ode45(@(t, y) linear_quadcopter(t, y, opts, hover_conditions), [0, tmax], inital_conds);
+% deviations from hover over time
+deltaPos   = y(:, 1:3);
+deltaVel   = y(:, 4:6);
+deltaPose  = y(:, 7:9);
+deltaOmega = y(:, 10:12);
+% recover absolue values over time
+x     = x0     + deltaPos;
+vel   = vel0   + deltaVel;
+pose  = pose0  + deltaPose;
+omega = omega0 + deltaOmega;
+
+plotQuadCopterDynamics(t, [x, vel, pose, omega])
+print(['img/', 'prob3d'], '-dpng')
+close
+
+% problem 3e: +0.1 rad/sec pitch rate
+% inital position deviation, velocity deviation
+deltaPos0   = [0 0 0]; % inertial displacement
+deltaVel0   = [0 0 0]; % body frame velocity
+deltaPose0  = [0 0 0]; % phi, theta, psi
+deltaOmega0 = [0 0.1 0]; % body angular velocity
+inital_conds = [deltaPos0, deltaVel0, deltaPose0, deltaOmega0];
+% there are currently no options implemented on the linearized model
+opts = struct('aero',          true, ... % aerodynamic forces
+              'control_motor', false);   % no motor control
+% call ode45 on the linearized model
+[t, y] = ode45(@(t, y) linear_quadcopter(t, y, opts, hover_conditions), [0, tmax], inital_conds);
+% deviations from hover over time
+deltaPos   = y(:, 1:3);
+deltaVel   = y(:, 4:6);
+deltaPose  = y(:, 7:9);
+deltaOmega = y(:, 10:12);
+% recover absolue values over time
+x     = x0     + deltaPos;
+vel   = vel0   + deltaVel;
+pose  = pose0  + deltaPose;
+omega = omega0 + deltaOmega;
+
+plotQuadCopterDynamics(t, [x, vel, pose, omega])
+print(['img/', 'prob3e'], '-dpng')
+close
+
+% problem 3f: +0.1 rad/sec yaw rate
+% inital position deviation, velocity deviation
+deltaPos0   = [0 0 0]; % inertial displacement
+deltaVel0   = [0 0 0]; % body frame velocity
+deltaPose0  = [0 0 0]; % phi, theta, psi
+deltaOmega0 = [0 0 0.1]; % body angular velocity
+inital_conds = [deltaPos0, deltaVel0, deltaPose0, deltaOmega0];
+% there are currently no options implemented on the linearized model
+opts = struct('aero',          true, ... % aerodynamic forces
+              'control_motor', false);   % no motor control
+% call ode45 on the linearized model
+[t, y] = ode45(@(t, y) linear_quadcopter(t, y, opts, hover_conditions), [0, tmax], inital_conds);
+% deviations from hover over time
+deltaPos   = y(:, 1:3);
+deltaVel   = y(:, 4:6);
+deltaPose  = y(:, 7:9);
+deltaOmega = y(:, 10:12);
+% recover absolue values over time
+x     = x0     + deltaPos;
+vel   = vel0   + deltaVel;
+pose  = pose0  + deltaPose;
+omega = omega0 + deltaOmega;
+
+plotQuadCopterDynamics(t, [x, vel, pose, omega])
+print(['img/', 'prob3f'], '-dpng')
+close
--- main.m
+++ main.m
@@ -1,148 +1,98 @@
-% ASEN 3128, Jacob Killelea, Assignment 2
+% ASEN 3128, Jacob Killelea, Assignment 3
 % Quadcopter sim
 clear
 clc
 close all
 addpath(genpath('util'));
 
-tmax = 1000; % seconds
+tmax = 100; % seconds2
 
-% inital position, velocity
-x0   = [0 0 0];
-vel0 = [0 0 0]; % body frame velocity
+% Problem 2a: +5 degrees bank
+x0     = [0 0 0]; % position
+vel0   = [0 0 0]; % body frame velocity
+pose0  = [deg2rad(5) 0 0]; % phi, theta, psi
+omega0 = [0 0 0]; % body angular velocity
+inital_conds = [x0, vel0, pose0, omega0];
 
-% pose (x, y, z rotation), and angular velocity
+opts = struct('aero',          true, ... % aerodynamic forces
+              'control_motor', false);   % no motor control
+% call ode45
+[t, y] = ode45(@(t, y) quadcopter(t, y, opts), [0, tmax], inital_conds);
+plotQuadCopterDynamics(t, y);
+print(['img/', 'prob2a'], '-dpng');
+close
+
+% Problem 2b: +5 degrees pitch
+x0     = [0 0 0]; % position
+vel0   = [0 0 0]; % body frame velocity
+pose0  = [0 deg2rad(5) 0]; % phi, theta, psi
+omega0 = [0 0 0]; % body angular velocity
+inital_conds = [x0, vel0, pose0, omega0];
+
+opts = struct('aero',          true, ... % aerodynamic forces
+              'control_motor', false);   % no motor control
+% call ode45
+[t, y] = ode45(@(t, y) quadcopter(t, y, opts), [0, tmax], inital_conds);
+plotQuadCopterDynamics(t, y);
+print(['img/', 'prob2b'], '-dpng');
+close
+
+% Problem 2c: +5 degrees azimuth
+x0     = [0 0 0]; % position
+vel0   = [0 0 0]; % body frame velocity
+pose0  = [0 0 deg2rad(5)]; % phi, theta, psi
+omega0 = [0 0 0]; % body angular velocity
+inital_conds = [x0, vel0, pose0, omega0];
+
+opts = struct('aero',          true, ... % aerodynamic forces
+              'control_motor', false);   % no motor control
+% call ode45
+[t, y] = ode45(@(t, y) quadcopter(t, y, opts), [0, tmax], inital_conds);
+plotQuadCopterDynamics(t, y);
+print(['img/', 'prob2c'], '-dpng');
+close
+
+% Problem 2d: +0.1 rad/sec roll rate
+x0     = [0 0 0]; % position
+vel0   = [0 0 0]; % body frame velocity
 pose0  = [0 0 0]; % phi, theta, psi
-omega0 = [0 0 0]; % body angular velocity
-
-% options and starting conditions
+omega0 = [0.1 0 0]; % body angular velocity
 inital_conds = [x0, vel0, pose0, omega0];
 
-opts = struct('aero',          false, ... % no aerodynamic forces
-              'control_motor', false); % no motor control
+opts = struct('aero',          true, ... % aerodynamic forces
+              'control_motor', false);   % no motor control
 % call ode45
 [t, y] = ode45(@(t, y) quadcopter(t, y, opts), [0, tmax], inital_conds);
-r     = y(:, 1:3);
-v     = y(:, 4:6);
-pose  = y(:, 7:9);
-omega = y(:, 10:12);
+plotQuadCopterDynamics(t, y);
+print(['img/', 'prob2d'], '-dpng');
+close
 
-figure; hold on; grid on;
-xlabel('Time (sec)');
-ylabel('Displacement (m)');
-title('Probelm 9 - Hover, No Aerodynamic Drag/Moments')
-plot(t, v(:, 1), 'DisplayName', 'u', 'LineWidth', 2) % body x velocity
-plot(t, v(:, 2), 'DisplayName', 'v', 'LineWidth', 2) % y
-plot(t, v(:, 3), 'DisplayName', 'w', 'LineWidth', 2) % z
-legend('show')
-
-% Probelm 10
-% options and starting conditions - hover
+% Problem 2e: +0.1 rad/sec pitch rate
+x0     = [0 0 0]; % position
+vel0   = [0 0 0]; % body frame velocity
+pose0  = [0 0 0]; % phi, theta, psi
+omega0 = [0 0.1 0]; % body angular velocity
 inital_conds = [x0, vel0, pose0, omega0];
-opts = struct('aero',                  true, ... % include aerodynamic forces
-              'control_motor', false);
+
+opts = struct('aero',          true, ... % aerodynamic forces
+              'control_motor', false);   % no motor control
 % call ode45
 [t, y] = ode45(@(t, y) quadcopter(t, y, opts), [0, tmax], inital_conds);
-r     = y(:, 1:3);
-v     = y(:, 4:6);
-pose  = y(:, 7:9);
-omega = y(:, 10:12);
+plotQuadCopterDynamics(t, y);
+print(['img/', 'prob2e'], '-dpng');
+close
 
-figure; hold on; grid on;
-xlabel('Time (sec)');
-ylabel('Displacement (m)');
-title('Probelm 10 - Hover, With Aerodynamic Drag/Moments')
-plot(t, v(:, 1), 'DisplayName', 'u', 'LineWidth', 2) % body x velocity
-plot(t, v(:, 2), 'DisplayName', 'v', 'LineWidth', 2) % y
-plot(t, v(:, 3), 'DisplayName', 'w', 'LineWidth', 2) % z
-legend('show')
-
-
-% Options and starting conditions - 5 m/s east
-% inital position, velocity
-x0   = [0 0 0];
-vel0 = [0 4.996497332 -0.0070045176]; % body frame velocity
-
-% pose (x, y, z rotation), and angular velocity
-pose0  = [0.0374330168 0 0]; % phi, theta, psi
-omega0 = [0 0 0]; % body angular velocity
-
-% options and starting conditions
+% Problem 2f: +0.1 rad/sec yaw rate
+x0     = [0 0 0]; % position
+vel0   = [0 0 0]; % body frame velocity
+pose0  = [0 0 0]; % phi, theta, psi
+omega0 = [0 0 0.1]; % body angular velocity
 inital_conds = [x0, vel0, pose0, omega0];
-opts = struct('aero',                  true, ... % include aerodynamic forces
-              'control_motor', true);
+
+opts = struct('aero',          true, ... % aerodynamic forces
+              'control_motor', false);   % no motor control
 % call ode45
 [t, y] = ode45(@(t, y) quadcopter(t, y, opts), [0, tmax], inital_conds);
-r     = y(:, 1:3);
-v     = y(:, 4:6);
-pose  = y(:, 7:9);
-omega = y(:, 10:12);
-
-figure; hold on; grid on;
-xlabel('Time (sec)');
-ylabel('Displacement (m)');
-title('Probelm 10 - Translation, With Aerodynamic Drag/Moments')
-plot(t, v(:, 1), 'DisplayName', 'u', 'LineWidth', 2) % body x velocity
-plot(t, v(:, 2), 'DisplayName', 'v', 'LineWidth', 2) % y
-plot(t, v(:, 3), 'DisplayName', 'w', 'LineWidth', 2) % z
-legend('show')
-
-% Options and starting conditions - 5 m/s north (psi = 90)
-% inital position, velocity
-x0   = [0 0 0];
-vel0 = [4.996497332, 0, -0.0070045176]; % body frame velocity
-
-% pose (x, y, z rotation), and angular velocity
-pose0  = [0, -0.0374330168, 90]; % phi, theta, psi
-omega0 = [0 0 0]; % body angular velocity
-
-% options and starting conditions
-inital_conds = [x0, vel0, pose0, omega0];
-opts = struct('aero',                  true, ... % include aerodynamic forces
-              'control_motor', true);
-% call ode45
-[t, y] = ode45(@(t, y) quadcopter(t, y, opts), [0, tmax], inital_conds);
-r     = y(:, 1:3);
-v     = y(:, 4:6);
-pose  = y(:, 7:9);
-omega = y(:, 10:12);
-
-figure; hold on; grid on;
-xlabel('Time (sec)');
-ylabel('Displacement (m)');
-title('Probelm 10 - Translation, \psi = 90, With Aerodynamic Drag/Moments')
-plot(t, v(:, 1), 'DisplayName', 'u', 'LineWidth', 2) % body x velocity
-plot(t, v(:, 2), 'DisplayName', 'v', 'LineWidth', 2) % y
-plot(t, v(:, 3), 'DisplayName', 'w', 'LineWidth', 2) % z
-legend('show')
-
-% hover test with inital upset
-% start with nose pitched 1 degree down, all other conditions set
-% to a stable hover
-% inital position, velocity
-x0   = [0 0 0];
-vel0 = [0 0 0]; % body frame velocity
-
-% pose (x, y, z rotation), and angular velocity
-pose0  = [deg2rad(5) -deg2rad(10) 0]; % phi, theta, psi
-omega0 = [0 0 0]; % body angular velocity
-
-% options and starting conditions
-inital_conds = [x0, vel0, pose0, omega0];
-opts = struct('aero',                  false, ... % no aerodynamic forces
-              'control_motor', false);
-% call ode45 (only to 10 seconds, better timescale to show the divergence
-[t, y] = ode45(@(t, y) quadcopter(t, y, opts), [0, 10], inital_conds);
-r     = y(:, 1:3);
-v     = y(:, 4:6);
-pose  = y(:, 7:9);
-omega = y(:, 10:12);
-
-figure; hold on; grid on;
-xlabel('Time (sec)');
-ylabel('Displacement (m)');
-title('Probelm 11 - Hover Attempt, \theta_0 = -10^\circ, \phi_0 = 5^\circ')
-plot(t, v(:, 1), 'DisplayName', 'u', 'LineWidth', 2) % body x velocity
-plot(t, v(:, 2), 'DisplayName', 'v', 'LineWidth', 2) % y
-plot(t, v(:, 3), 'DisplayName', 'w', 'LineWidth', 2) % z
-legend('show')
+plotQuadCopterDynamics(t, y);
+print(['img/', 'prob2f'], '-dpng');
+close
