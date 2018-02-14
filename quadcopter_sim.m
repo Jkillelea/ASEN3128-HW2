1f0e0ca5615b61ae2c53c914e3a7a2ffbcfa44f5
Jacob Killelea <Jkillelea@protonmail.ch>
2018-02-14 07:25:35 -0700

    update for new data format

--- quadcopter_sim.m
+++ quadcopter_sim.m
@@ -3,44 +3,38 @@
 clear; close all; clc;
 addpath(genpath('util'));
 
-load('RSdata.mat'); % [rt_tout, rt_yout]
+% rt_calib, rt_cmd, rt_estim, rt_motor, rt_optical, rt_posref, rt_sensor
+% rt_tout, rt_yout
+load('RSdata_Drone09_1330.mat');
 
-signals = rt_yout.signals;
+% The columns of values are organized with columns:
+% X Y Z yaw pitch roll dx dy dz p q r
 
-% Field labels
-% [empty], <X>, <Y>, <Z>, <yaw>, <pitch>, <roll>, <dx>, <dy>, <dz>, <p>, <q>,
-% <r>, [empty], [empty], <ddx>, <ddy>, <ddz>, <p>, <q>, <r>, <altitude_sonar>,
-% <prs>, [empty], [empty], [empty], [empty], [empty], [empty]
+t = rt_sensor.time;
+X     = rt_estim.signals.values(:, 1);
+Y     = rt_estim.signals.values(:, 2);
+Z     = rt_estim.signals.values(:, 3);
+yaw   = rt_estim.signals.values(:, 4); % radians
+pitch = rt_estim.signals.values(:, 5);
+roll  = rt_estim.signals.values(:, 6);
+dx    = rt_estim.signals.values(:, 7);
+dy    = rt_estim.signals.values(:, 8);
+dz    = rt_estim.signals.values(:, 9);
+p     = rt_estim.signals.values(:, 10);
+q     = rt_estim.signals.values(:, 11);
+r     = rt_estim.signals.values(:, 12);
 
-x   = getDataByLabel(signals, '<X>');
-y   = getDataByLabel(signals, '<Y>');
-z   = getDataByLabel(signals, '<Z>');
+% convert [X, Y, Z] to [N, E, D]
+sz = length(X);
+N = zeros(sz, 1);
+E = zeros(sz, 1);
+D = zeros(sz, 1);
+for i = 1:sz
+  Qeb  = R1(-roll(i))*R2(-pitch(i))*R3(-yaw(i)); % body vector to inertial vector
+  tmp  = Qeb * [X(i); Y(i); Z(i)];
+  N(i) = tmp(1);
+  E(i) = tmp(2);
+  D(i) = tmp(3);
+end
 
-dx  = getDataByLabel(signals, '<dx>');
-dy  = getDataByLabel(signals, '<dy>');
-dz  = getDataByLabel(signals, '<dz>');
-
-ddx = getDataByLabel(signals, '<ddx>');
-ddy = getDataByLabel(signals, '<ddy>');
-ddz = getDataByLabel(signals, '<ddz>');
-
-z = -z;
-dz = -dz;
-ddz = -ddz;
-
-% print out flightpath
-figure; hold on;
-plot3(x, y, z)
-plot3(x(1), y(1), z(1), 'ro')
-xlabel('x')
-ylabel('y')
-zlabel('z')
-xlim([-1, 1])
-ylim([-1, 1])
-
-% velocity vectors
-% for i = 1:10:length(dx)
-%   line( [x(i), x(i) + 0.1*dx(i)], ...
-%         [y(i), y(i) + 0.1*dy(i)], ...
-%         [z(i), z(i) + 0.1*dz(i)])
-% end
+plotQuadCopterDynamics(t, [N, E, D, dx, dy, dz, roll, pitch, yaw, p, q, r])
